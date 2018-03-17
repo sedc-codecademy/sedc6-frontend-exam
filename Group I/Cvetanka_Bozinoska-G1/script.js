cca0dada3536d47223b050d12da689a6140a51fd
ceceLibere <35581184+ceceLibere@users.noreply.github.com>
2018-03-17 21:23:15 +0100

    Update script.js

--- Group I/Cvetanka_Bozinoska-G1/script.js
+++ Group I/Cvetanka_Bozinoska-G1/script.js
@@ -1,57 +1,50 @@
-$(document).ready(() => {
-
-
-let bands = []; 
-let tempBands = [];
+let bandList = [];
+let tempbandList = [];
 let page = 1;
-let maxsize = 10;
+let maxBends = 10;
 let sort = "";
-let myTags = [];
-    
-    $.ajax({
-        method: "GET",
-        url: "https://raw.githubusercontent.com/sedc-codecademy/sedc6-frontend-exam/master/band-data.json",
+let tagList = [];
 
-        success: (data) => {
-            tempBands = bands;
-            bands = JSON.parse(data);
-            
-            for(let i = 0; i < bands.length; i += 1){
-                for(let j = 0; j < bands[i].tags.length; j += 1){
-                    myTags.push(bands[i].tags[j]);
-                }
-            myTags = Array.from( new Set(myTags));
-                
-            for(let i =0; i < myTags.length; i += 1){
-                $("#tags").append(`
-                <oprion value="${myTags[i]}">${myTags[i]}</option>
-            `);
+$(document).ready(() => {
+   
+$.ajax({
+    method: "GET",
+    url: "https://raw.githubusercontent.com/sedc-codecademy/sedc6-frontend-exam/master/band-data.json",
+    success: (data) => {
+        bandList = JSON.parse(data);
+        tempbandList = bandList;
+
+        for(let i = 0; i < bandList.length; i += 1){
+            for(let j = 0; j < bandList[i].tags.length; j ++){
+				let check = 0;
+			    for(let k = 0; k < tagList.length; k ++) {
+				    if(bandList[i].tags[j] == tagList[k]) {
+						check = 1;
+                    }						
+				}
+				if(check == 0) {
+					tagList.push(bandList[i].tags[j]);
+	                $("#tags").append(`
+                    <option value="${tagList[i]}">${bandList[i].tags[j]}</option>
+                    `);
+				}
             }
         }
-            // for (let i = 0; i < bands.length; i++) {
-            //     bands[i].id = (i + 1);
+        showData(bandList, page, maxBends)
+    },
+    error: (err) => {
+        console.log(err);
+    }
+});
 
-            // } 
-
-            localStorage["bands"] = JSON.stringify(bands);
-            bands = JSON.parse(localStorage["bands"]);
-            //filterTable(bands);
-            showData(bands, page, maxsize)
-        },
-        error: (err) => {
-            console.log(err);
-        }
-    });
-   
-    
-    $("#sortName").on("click", () => {
-        if(sort === "name1"){
-            sort = "name2"
+ $("#sortName").on("click", () => {
+        if(sort === "nameA"){
+            sort = "nameZ"
         }
         else{
-            sort = "name1"
+            sort = "nameA"
         }
-        showData(bands, page, maxsize);
+        showData(bandList, page, maxBends);
     });
     $("#sortAlbums").on("click", () => {
         if(sort === "albums1"){
@@ -60,105 +53,94 @@ let myTags = [];
         else{
             sort = "albums1"
         }
-        showData(bands, page, maxsize);
+        showData(bandList, page, maxBends);
     });
-    $("#searchFilter, #active, #tags").on("change", () => {
-        showData(bands, page, maxEsize);
+    $("#srcFilter, #isActive, #tags").on("change", () => {
+        showData(bandList, page, maxBends);
     });
     $("#prev").on("click", () => {
         if(page > 1){
             page -= 1;
         }
-        showData(bands, page, maxsize);
+        showData(bandList, page, maxBends);
     });
     $("#next").on("click", () => {
-        if(page < Math.ceil(tempBands.length / maxsize)){
+        if(page < Math.ceil(tempbandList.length / maxBends)){
             page += 1;
         }
-        showData(bands, page, maxsize);
+        showData(bandList, page, maxBends);
     });
 
 
-    function populateTable(bands) {
-        // console.log(data);
-        console.log(bands);
-        $("#myBody").html("");
+function showData(data, page, maxBends){
+    $("#myTable").html("");
+    tempbandList = filterData(data);
+    tempbandList = sortData(tempbandList);
+    let maxPage = Math.ceil(tempbandList.length / maxBends);
+    let count = maxPage === page ? tempbandList.length : maxBends;
+    $('#currentPage').text(page);
+    for(let i = (page - 1) * maxBends; i < count; i ++){
+        populateTable(tempbandList[i], i + 1);
+    }
+}
 
-        for (let i = 0; i < bands.length; i++) {
-
-            let tags = "";
-
-            for (let j = 0; j < bands[i].tags.length; j++) {
-                tags += ` ${bands[i].tags[j]}, `
-            }
-            console.log(tags);
-            let members = "";
-            for (let j = 0; j < bands[i].members.length; j++) {
-                if(bands.members[j].former === true){
-
-                }
-                members += `${bands[i].members[j].name}</br> `
-            }
-
-
-
-             $("#myBody").append(`
-                            <tr>
-                                <td id="id${i}">${id} </td>
-                                <td id="name${i}">${bands[i].name}</td>
-                                <td id="active${i}">${bands[i].active}</td>
-                                <td id="tags${i}">${tags} </td>
-                               <td id="${i}">${members}</td>
-                                <td id="length${i}">${bands[i].albums.length}</td>
-                            </tr>`);
-
-             }
-           
-        }
+function filterData(data){
+    data = data.filter(item => item.name.toLowerCase().includes($("#srcFilter").val().toLowerCase()));
+    let selectTag = $("#tags").find(":selected").text();
     
+	if(selectTag != 'All') {
+        data = data.filter(item => item.tags.includes(selectTag));
+	}
+    
+    if($("#isActive").is(":checked")){
+        data = data.filter(item => item.active === true);
+    }
+	return data;
+}
 
+function sortData(data){
+    switch(sort){
+        case "nameA":
+            data.sort((a, b) => a.name.localeCompare(b.name));
+            break;
+        case "nameZ":
+            data.sort((a, b) => b.name.localeCompare(a.name));
+            break;
+        case "albums1":
+            data.sort((a, b) => a.albums.length - b.albums.length);
+            break;
+        case "albums2":
+            data.sort((a, b) => b.albums.length - a.albums.length);
+            break;
+        default:
+            break;
+    }
+    return data;
+}
 
-        function showData(bands, page, maxsize){
-            $("#myTable").html("");
-            filterData(bands);
-            tempBands = sortData(tempBands);
-            let maxPage = Math.ceil(tempBands.length / maxsize);
-            let count = maxPage === page ? tempBands.length : maxsize;
-        
-            for(let i = (page - 1) * maxsize; i < count; i += 1){
-                populateTable(tempBands[i], i + 1);
-            }
-        }
-        
-        function filterData(data){
-            tempBands = data.filter(n => n.name.toLowerCase().includes($("#searchFilter").val().toLowerCase()));
-            let selectedTag = $("#tags").find(":selected").text();
-            console.log(selectedTag);
-            tempBands = tempBands.filter(n => n.tags.includes(selectedTag));
-            console.log(tempBands);
-            if($("#isActive").is(":checked")){
-                tempBands = tempBands.filter(n => n.active === true);
-            }
-        }
-        
-        function sortData(bands){
-            switch(sort){
-                case "nameAsc":
-                    bands.sort((a, b) => a.name.localeCompare(b.name));
-                    break;
-                case "nameDes":
-                    bands.sort((a, b) => b.name.localeCompare(a.name));
-                    break;
-                case "albumsAsc":
-                    bands.sort((a, b) => a.albums.length - b.albums.length);
-                    break;
-                case "albumsDes":
-                    bands.sort((a, b) => b.albums.length - a.albums.length);
-                    break;
-                default:
-                    break;
-            }
-            return bands;
-        }
+function populateTable(data, id){
+    let tags = "";
+    let members = "";
+    for(let i = 0; i < data.tags.length; i ++){
+		if(i+1 == data.tags.length) {
+			tags += `${data.tags[i]}`;
+		} else {
+            tags += `${data.tags[i]}, `;
+		}
+    }
+    for(let i = 0; i < data.members.length; i ++){
+        members += `${data.members[i].name}</br>`;
+    }
+    $("#myTable").append(`
+        <tr>
+            <td>${id}</td>
+            <td>${data.name}</td>
+            <td>${data.active}</td>
+            <td>${tags}</td>
+            <td>${members}</td>
+            <td>${data.albums.length}</td>
+        </tr>
+    `);
+}
 
-    });  
+});
